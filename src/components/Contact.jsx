d17513281eb5a7ddb551efda9e5fca43b4160e13
aggarwalkamal123 <aggarwalkamal782@gmail.com>
2026-08-16 17:46:17 +0530

    added insta link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ const socialLinks = [
     },
     {
         name: "Instagram",
-        href: "https://www.instagram.com/",
+        href: "https://www.instagram.com/Kamalaggarwal2",
         icon: Camera,
     },
 ];
